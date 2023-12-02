e0e92773d66625f07d7d8a2971e5ba08b881d40e
Supernova <ldorval@gmail.com>
2023-12-02 10:24:12 -0500

    Day 1 part 2

--- Day01/Day01.cs
+++ Day01/Day01.cs
@@ -2,11 +2,28 @@
 
 public class Day01
 {
-    public static int Solve(List<string> input)
+    public static int SolvePart1(List<string> input) => input.Sum(x => Convert.ToInt32(GetFirstDigit(x).ToString() + GetLastDigit(x)));
+
+    public static object SolvePart2(List<string> input)
     {
-        return input.Sum(x => Convert.ToInt32(GetFirstDigit(x).ToString() + GetLastDigit(x)));
+        var cleanedInput = input.Select(CleanInput).ToList();
+        return SolvePart1(cleanedInput);
     }
 
     public static int GetFirstDigit(string s) => s.First(Char.IsNumber) - '0';
     public static int GetLastDigit(string s) => s.Last(Char.IsNumber) - '0';
+
+    public static string CleanInput(string s)
+    {
+        return s
+            .Replace("one", "one1one")
+            .Replace("two", "two2two")
+            .Replace("three", "three3three")
+            .Replace("four", "four4four")
+            .Replace("five", "five5five")
+            .Replace("six", "six6six")
+            .Replace("seven", "seven7seven")
+            .Replace("eight", "eight8eight")
+            .Replace("nine", "nine9nine");
+    }
 }
--- Day01/Day01Test.cs
+++ Day01/Day01Test.cs
@@ -1,18 +1,16 @@
 ﻿using FluentAssertions;
-using NUnit.Framework;
 
 namespace AdventOfCode2023.Day01;
 
 public class Day01Test
 {
-    private List<string> input = new() { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" };
-
+    private List<string> inputPart1 = new() { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" };
+    private List<string> inputPart2 = new() { "two1nine","eightwothree","abcone2threexyz","xtwone3four","4nineeightseven2", "zoneight234","7pqrstsixteen" };
+    
     [Test]
     public void GetsFirstDigit()
     {
         Day01.GetFirstDigit("1abc2").Should().Be(1);
-        Day01.GetFirstDigit("pqr3stu8vwx").Should().Be(3);
-        Day01.GetFirstDigit("a1b2c3d4e5f").Should().Be(1);
         Day01.GetFirstDigit("treb7uchet").Should().Be(7);
     }
 
@@ -20,20 +18,38 @@ public class Day01Test
     public void GetsLastDigit()
     {
         Day01.GetLastDigit("1abc2").Should().Be(2);
-        Day01.GetLastDigit("pqr3stu8vwx").Should().Be(8);
-        Day01.GetLastDigit("a1b2c3d4e5f").Should().Be(5);
         Day01.GetLastDigit("treb7uchet").Should().Be(7);
+    }
+
+    [Test]
+    public void CleansInput()
+    {
+        Day01.CleanInput("eightwothree").Should().Be("eight8eightwo2twothree3three");
+        Day01.CleanInput("4nineeightseven2").Should().Be("4nine9nineeight8eightseven7seven2");
+        Day01.CleanInput("7pqrstsixteen").Should().Be("7pqrstsix6sixteen");
     }
     
     [Test]
     public void ExamplePart1()
     {
-        Day01.Solve(input).Should().Be(142);
+        Day01.SolvePart1(inputPart1).Should().Be(142);
+    }
+    
+    [Test]
+    public void ExamplePart2()
+    {
+        Day01.SolvePart2(inputPart2).Should().Be(281);
     }
 
     [Test]
     public void SolutionPart1()
     {
-        Console.Write(Day01.Solve("Day01".ReadAll().LinesToString()));
+        Console.Write(Day01.SolvePart1("Day01".ReadAll().LinesToString()));
+    }
+    
+    [Test]
+    public void SolutionPart2()
+    {
+        Console.Write(Day01.SolvePart2("Day01".ReadAll().LinesToString()));
     }
 }
