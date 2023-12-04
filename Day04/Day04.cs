3a5a8dbd1abd66cdb7f48651f75428a143ab86d5
Supernova <ldorval@gmail.com>
2023-12-04 12:27:54 -0500

    Day 04

---
+++ Day04/Day04.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode2023.Day04;
+
+public class Day04
+{
+    public static int Solve(List<string> input)
+    {
+        return input
+            .Select(ExtractCard)
+            .Select(x => GetCardScore(MatchingNumbersCount(x)))
+            .Sum();
+    }
+
+    public static int SolvePart2(List<string> input)
+    {
+        var cardsIdCount = input.Select(ExtractCard).ToDictionary(x => x.Id, _ => 1);
+
+        foreach (var card in input.Select(ExtractCard))
+        {
+            for (var i = 1; i <= MatchingNumbersCount(card); i++)
+            {
+                cardsIdCount[card.Id + i] += cardsIdCount[card.Id];
+            }
+        }
+
+        return cardsIdCount.Sum(x => x.Value);
+    }
+
+    private static int MatchingNumbersCount(Card x) => x.Numbers.Intersect(x.WinningNumbers).Count();
+    public static int GetCardScore(int matchingNumbersCount) => (int)Math.Pow(2, matchingNumbersCount - 1);
+
+    public static Card ExtractCard(string line)
+    {
+        var numbers = line.Split(":")[1];
+        var split = numbers.Split("|");
+        return new Card(
+            Convert.ToInt32(line.Split(":")[0].Replace("Card ", "")),
+            split[0].Trim().Split(" ").Where(x => x != "").Select(x => Convert.ToInt32(x)).ToList(),
+            split[1].Trim().Split(" ").Where(x => x != "").Select(x => Convert.ToInt32(x)).ToList()
+        );
+    }
+
+    public record Card(int Id, List<int> WinningNumbers, List<int> Numbers);
+}
---
+++ Day04/Day04Test.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+
+namespace AdventOfCode2023.Day04;
+
+public class Day04Test
+{
+    [Test]
+    public void ExamplePart1() => Day04.Solve("Day04Example".ReadAll().AsListOfStrings()).Should().Be(13);
+
+    [Test]
+    public void ExamplePart2() => Day04.SolvePart2("Day04Example".ReadAll().AsListOfStrings()).Should().Be(30);
+
+    [Test]
+    public void SolutionPart1() => Console.Write(Day04.Solve("Day04".ReadAll().AsListOfStrings()));
+
+    [Test]
+    public void SolutionPart2() => Console.Write(Day04.SolvePart2("Day04".ReadAll().AsListOfStrings()));
+
+    [Test]
+    public void ExtractsCardFromLine()
+    {
+        Day04.ExtractCard("Card 32: 41 48 83 86 17 | 83 86  6 31 17  9 48 53").Should().BeEquivalentTo(new Day04.Card(
+            32, new List<int> { 41, 48, 83, 86, 17 }, new List<int> { 83, 86, 6, 31, 17, 9, 48, 53 })
+        );
+    }
+
+    [Test]
+    public void GetsCardScore()
+    {
+        Day04.GetCardScore(1).Should().Be(1);
+        Day04.GetCardScore(2).Should().Be(2);
+        Day04.GetCardScore(4).Should().Be(8);
+        Day04.GetCardScore(5).Should().Be(16);
+    }
+}
